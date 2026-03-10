b3c219cdc5bd225709ee180b21bb4dca29031c0a
tomines-jaeyoung <tominesss12@gmail.com>
2026-03-10 11:31:41 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/kim/bookmarket2/BookMarket2Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.kim.bookmarket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarket2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarket2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/kim/bookmarket2/BookMarket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.kim.bookmarket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarket2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
