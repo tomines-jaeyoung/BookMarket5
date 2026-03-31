98db77a3ca731814b17c95e8d2d942a1e0614fd7
tomines-jaeyoung <tominesss12@gmail.com>
2026-03-31 09:23:49 +0900

    books 완성

--- src/main/java/kr/ac/kopo/kim/bookmarket2/controller/BookController.java
+++ src/main/java/kr/ac/kopo/kim/bookmarket2/controller/BookController.java
@@ -5,20 +5,31 @@ import kr.ac.kopo.kim.bookmarket2.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String requestBookList(Model model){
         List<Book> listOfBooks = bookService.getAllBookList();
         model.addAttribute("bookList", listOfBooks);
         return "books";
+    }
+    @GetMapping("/all")
+    public ModelAndView requestAllBooks(){
+     ModelAndView modelAndView = new ModelAndView();
+     List<Book> list = bookService.getAllBookList();
+     modelAndView.addObject("bookList", list);
+     modelAndView.setViewName("books");
+     return modelAndView;
     }
 }
