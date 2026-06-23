fe4b4191eb9d5be4345135b83be6ce866a39e1f8
tomines-jaeyoung <tominesss12@gmail.com>
2026-06-23 10:22:59 +0900

    10장 SecurityConfig BookMarket 에 도서등록페이지에 로그인및 로그아웃 설정

---
+++ src/main/java/kr/ac/kopo/kim/bookmarket2/controller/LoginController.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.kim.bookmarket2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/loginfailed")
+    public String loginfailed(Model model){
+        model.addAttribute("error", "true");
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(){
+        return "login";
+    }
+}
